2c16e51b21f039cd123fc7bea597491c4b821716
nicholas-dev2 <nicholas.m@escolar.ifrn.edu.br>
2026-06-22 22:41:27 -0300

    Calculate and display individual share of bill

--- lista-01/q11.py
+++ lista-01/q11.py
@@ -1,1 +1,8 @@
+consumo = float(input("Valor total da conta: "))
+pessoas = int(input("Pessoas na mesa: "))
 
+consumo += consumo * 0.1
+
+divisao = consumo / pessoas
+
+print("Cada amigo deverá pagar R$", divisao)
