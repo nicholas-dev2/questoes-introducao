d7ba6ba3ba278860d0f70bdc1b2b25cff8652a47
nicholas-dev2 <nicholas.m@escolar.ifrn.edu.br>
2026-06-22 22:41:49 -0300

    Update q12.py

--- lista-01/q12.py
+++ lista-01/q12.py
@@ -1,1 +1,9 @@
+capacidade = float(input("Capacidade do tanque (L): "))
+consumo = float(input("Consumo do carro (km/L): "))
+preco = float(input("Preço da gasolina: "))
 
+distancia = capacidade * consumo
+valor = capacidade * preco
+
+print("Distância total com o tanque cheio:", distancia, "km")
+print("Valor para encher o tanque: R$", valor)
