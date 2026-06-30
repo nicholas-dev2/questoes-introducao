a6bd3cd2de1a80782167649aa33ac8e6254f2e48
nicholas-dev2 <nicholas.m@escolar.ifrn.edu.br>
2026-06-29 21:59:04 -0300

    Update q1.py

--- lista-02/q1.py
+++ lista-02/q1.py
@@ -1,1 +1,8 @@
+segundos = int(input("Insira a quantidade de segundos: "))
+dias = segundos // 86400
+horas = segundos // 3600
+minutos = segundos // 60
 
+print(f"Dias: {dias}")
+print(f"Horas: {horas}")
+print(f"Minutos: {minutos}")
